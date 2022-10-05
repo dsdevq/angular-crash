fa4df1b64216e3c18d7b1c05dbfeef028152deec
Denis <payar3282@gmail.com>
2022-10-05 18:43:49 +0300

    localstorage added

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,25 +1,21 @@
 import { TodoService } from './../../services/todo.service';
 import { ITodo } from './../../models/todo';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss'],
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent {
   @Input() todo!: ITodo;
 
   constructor(private todoService: TodoService) {}
 
   toggleCompleted(id: number) {
     this.todoService.changeCompleted(id);
-    console.log(id);
-    console.log(this.todo.completed);
   }
   removeTodo(id: number) {
     this.todoService.removeTodo(id);
   }
-
-  ngOnInit(): void {}
 }
